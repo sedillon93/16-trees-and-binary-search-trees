6056b37f9efe369067f93415d90e0dafdedbf593
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-19 13:51:26 -0800

    write basic Stack class

---
+++ lab-shannon/lib/stack.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class Stack{
+  constructor{
+    this.data = [];
+  }
+
+  push(value){
+    this.data.push(value);
+  }
+
+  pop(){
+    return this.data.pop();
+  }
+}
+
+module.exports = Stack;
