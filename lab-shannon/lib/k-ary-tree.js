dc23be4d0ae15161d7718bfa482f652d457184d9
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-19 14:57:07 -0800

    attach methods to prototype without using arrow functions since they don't have their own 'this'

--- lab-shannon/lib/k-ary-tree.js
+++ lab-shannon/lib/k-ary-tree.js
@@ -1,16 +1,18 @@
 'use strict';
-// let Queue = require(`./queue`);
-// let Stack = require(`./stack`);
+
+let Queue = require(`./queue`);
+let Stack = require(`./stack`);
 
 function KAryTree(value){
   this.value = value;
   this._children = [];
 }
 
-KAryTree.prototype.appendChild = (child) => {
+KAryTree.prototype.appendChild = function(child){
   if(!(child instanceof KAryTree)){
     throw new TypeError(`The value to append must be a K-ary Tree`);
   }
+  console.log(this);
   this._children.push(child);
 };
 
@@ -23,3 +25,5 @@ KAryTree.prototype.toString = function(str){    // breadth (queue)
 KAryTree.prototype.toArray = function(array){     //depth (stack)
 
 };
+
+module.exports = KAryTree;
