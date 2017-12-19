91a99142a875f2a913d45df5af69f954e0c2303e
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-19 13:58:42 -0800

    write first test for find method's success case

--- lab-shannon/__test__/k-ary-tree.test.js
+++ lab-shannon/__test__/k-ary-tree.test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+describe(`K-Ary Tree`, () => {
+  test(`find method should return the first node matching the specified value if no errors are present`, () => {
+    let four = new KAryTree(4);
+    let three = new KAryTree(3);
+    let seven = new KAryTree(7);
+    let fifteen = new KAryTree(15);
+    let two = new KAryTree(2);
+    let nine = new KAryTree(9);
+
+    four.appendChild(three);
+    three.appendChild(seven);
+    three.appendChild(fifteen);
+    three.appendChild(two);
+    two.appendChild(nine);
+
+    expect(four.find(7).value).toEqual(7);
+  });
+});
--- lab-shannon/lib/k-ary-tree.js
+++ lab-shannon/lib/k-ary-tree.js
@@ -5,7 +5,6 @@
 function KAryTree(value){
   this.value = value;
   this._children = [];
-
 }
 
 KAryTree.prototype.appendChild = (child) => {
