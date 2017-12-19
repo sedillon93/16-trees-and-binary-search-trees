81e7e563a2e190633eb58289aee0cd8da7c0590e
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-18 16:45:04 -0800

    wrtie insert method on binary search tree (from class)

--- lab-shannon/lib/binary-search-tree.js
+++ lab-shannon/lib/binary-search-tree.js
@@ -1,1 +1,34 @@
 'use strict';
+
+class BinarySearchTree{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value){
+    if(typeof value !== 'number'){
+      throw new TypeError(`You can only insert numbers`);
+    }
+    if(this.value === value){
+      throw new TypeError(`That value is already present; You cannot create duplicate numbers`);
+    }
+    if(this.value > value){
+      if(!this.left){
+        this.left = new BinarySearchTree(value);
+        return;
+      }
+      this.left.insert(value);
+      return;
+    }
+    if(this.value < value){
+      if(!this.right){
+        this.right = new BinarySearchTree(value);
+        return;
+      }
+      this.right.insert(value);
+      return;
+    }
+  }
+}
