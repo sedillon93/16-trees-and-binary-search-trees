df96bffe4b9f0bec6ed8bfe1c30adb1805e2edb8
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-19 13:48:32 -0800

    create basic Queue class

---
+++ lab-shannon/lib/queue.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class Queue{
+  constructor{
+    this.data = [];
+  }
+
+  enqueue(value){
+    this.data.push(value);
+  }
+
+  dequeue(){
+    return this.data.shift();
+  }
+}
+
+module.exports = Queue;
